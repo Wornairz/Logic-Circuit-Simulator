11d67b431b2bf7f45e9babebf564bff0f2c49712
Giuseppe Patané <patanegiuseppe7@gmail.com>
2018-12-15 11:39:56 +0100

    Modificato l'inserimento dei componenti
    Modificato l'inserimento dei componenti e riorganizzato il codice

--- src/app/content/wire.ts
+++ src/app/content/wire.ts
@@ -1,29 +1,63 @@
+import { Pin } from './pin';
+import { position } from '../globals';
+
 export class Wire {
-    public sorgente: { x: number, y: number };
-    public destinazione: { x: number, y: number };
+    public sorgente: Pin;
+    public destinazione: Pin;
     public static spessore = 2.5;
     public static colore = '#000000';
     public spostamento_orizzontale: boolean;
-    constructor(sorgente, destinazione, spostamento_orizzontale) {
-        this.sorgente = sorgente;
-        this.destinazione = destinazione;
+
+
+    constructor(sorgente: position, destinazione: position , spostamento_orizzontale: boolean) {
+        this.sorgente = new Pin(sorgente);
+        this.destinazione = new Pin(destinazione);
         this.spostamento_orizzontale = spostamento_orizzontale;
     }
+
     public collide(x: number, y: number): boolean {
-        let a = this.sorgente.x;
-        let b = this.destinazione.y;
+        let a = this.sorgente.posizione.x;
+        let b = this.destinazione.posizione.y;
         if (this.spostamento_orizzontale) {
-            a = this.destinazione.x;
-            b = this.sorgente.y;
+            a = this.destinazione.posizione.x;
+            b = this.sorgente.posizione.y;
         }
         if (x >= a - 2.5 * Wire.spessore && x <= a + 2.5 * Wire.spessore) {
-            if ((y >= this.sorgente.y && y <= this.destinazione.y) || (y >= this.destinazione.y && y <= this.sorgente.y)) {
+            if ((y >= this.sorgente.posizione.y && y <= this.destinazione.posizione.y) || (y >= this.destinazione.posizione.y && y <= this.sorgente.posizione.y)) {
                 return true;
             }
         } else if (y >= b - 2.5 * Wire.spessore && y <= b + 2.5 * Wire.spessore) {
-            if ((x >= this.sorgente.x && x <= this.destinazione.x) || (x >= this.destinazione.x && x <= this.sorgente.x)) {
+            if ((x >= this.sorgente.posizione.x && x <= this.destinazione.posizione.x) || (x >= this.destinazione.posizione.x && x <= this.sorgente.posizione.x)) {
                 return true;
             }
         } else { return false; }
+    }
+
+    public draw(context: CanvasRenderingContext2D) {
+        // Inizia un nuovo percorso
+        context.beginPath();
+
+        context.lineWidth = Wire.spessore;
+        context.strokeStyle = Wire.colore;
+        // Si inizia a disegnare dalle coordinate di sorgente...
+        context.moveTo(this.sorgente.posizione.x, this.sorgente.posizione.y);
+        // ... fino alle coordinate della x di destinazione (linea orizzontale)
+
+        // Se lo spostamento tende verso destra, allora si disegna prima una linea orizzontale
+        if (this.spostamento_orizzontale) {
+            context.lineTo(this.destinazione.posizione.x, this.sorgente.posizione.y);
+        } else {
+            context.lineTo(this.sorgente.posizione.x, this.destinazione.posizione.y);
+        }
+
+        context.lineTo(this.destinazione.posizione.x, this.destinazione.posizione.y);
+        // Dopo si disegna effettivamente
+        context.stroke();
+
+        // Si disegnano i due quadrati alle estremità del percorso
+        context.beginPath();
+        this.sorgente.draw(context);
+        this.destinazione.draw(context);
+        context.stroke();
     }
 }
--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 
 Injectable()
-export class Globals{
+export class Globals {
     public static spazio_linee: number = 20;
+    public static width: number = 1520;
+    public static height: number = 750;
 }
+export type position = { x: number, y: number };
